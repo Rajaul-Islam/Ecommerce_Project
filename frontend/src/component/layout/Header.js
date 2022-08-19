8440ba603c1fa16f1e508f8d58b33de089256c19
rajaul rabbi <Rajaul-Islam>
2022-08-19 11:22:43 +0600

    frontend create

---
+++ frontend/src/component/layout/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {ReactNavbar} from "overlay-navbar"
+const Header = () => {
+  return (
+   <ReactNavbar>
+    
+   </ReactNavbar>
+  )
+}
+
+export default Header
